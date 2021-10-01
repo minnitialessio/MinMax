c437e690071049658d468a19cc83a9bba873b98f
Alessio Minniti <ALESSIO.MINNITI@local.ittcesena.it>
2021-10-01 09:31:42 +0200

    V 1.1: Aggiunta calcolo minimo

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -10,6 +10,20 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5] { 3, 6, -3, 5, 7 };
+
+            int min = array[0];
+
+            for (int i=1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            Console.WriteLine(min);
+            Console.ReadKey();
         }
     }
 }
